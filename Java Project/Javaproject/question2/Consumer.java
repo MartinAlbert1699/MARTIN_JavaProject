1726f436aadc0d55bbf0058708cbf9aabb970e49
MartinAlbert1699 <139337463+MartinAlbert1699@users.noreply.github.com>
2023-10-31 13:33:41 +0530

    Add files via upload

---
+++ Java Project/Javaproject/question2/Consumer.java
@@ -0,0 +1,34 @@
+package com.Javaproject.question2;
+
+
+
+public class Consumer extends Thread {
+
+	Thread1 p;
+
+	Consumer(Thread1 p)
+
+	{
+
+		this.p = p;
+
+		start();
+
+	}
+
+	public void run()
+
+	{
+		int i = 0;
+
+		while (i == 0)
+
+		{
+			i++;
+			p.deSerializeProjectDetails();
+
+		}
+
+	}
+
+}
